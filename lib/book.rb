3ce883eaf0573030277f3d2492a17726d64bf7c3
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-19 11:37:44 +0300

    updates

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,16 @@
 class Book
-    attr_accessor :title, :author, :page_count, :genre
+    attr_accessor :title, :page_count, :genre, :author
   
     def initialize(title)
       @title = title
     end
   
     def turn_page
-        puts "Flipping the page...wow, you read fast!"
-      end
+      @page_count ||= 0
+      @page_count += 1
+      puts "Flipping the page...wow, you read fast!"
+    end
   end
   
-
+  book = Book.new("And Then There Were None")
+  book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,68 +1,12 @@
-# shoe.rb
+class Shoe 
+    attr_accessor :brand, :color, :size, :material, :condition
+def initialize (brand)
+    @brand = brand
+end
 
-class Shoe
-    attr_reader :brand, :color, :size, :material, :condition
-  
-    def initialize(brand)
-      @brand = brand
-      @color = nil
-      @size = nil
-      @material = nil
-      @condition = nil
-    end
-  
-    def cobble
-      @condition = "new"
-      puts "The shoe has been repaired."
-    end
-  end
-  
-
-  # 02_shoe_spec.rb
-
-require_relative 'path_to_shoe_file/shoe'
-
-RSpec.describe Shoe do
-  describe "#new" do
-    it "gets initialized with a brand" do
-      expect { Shoe.new("Adidas") }.not_to raise_error
-    end
-  end
-
-  describe "properties" do
-    let(:shoe) { Shoe.new("Nike") }
-
-    it "has a brand" do
-      expect(shoe.brand).to eq("Nike")
-    end
-
-    it "has a color" do
-      expect(shoe.color).to be_nil
-    end
-
-    it "has a size" do
-      expect(shoe.size).to be_nil
-    end
-
-    it "has a material" do
-      expect(shoe.material).to be_nil
-    end
-
-    it "has a condition" do
-      expect(shoe.condition).to be_nil
-    end
-  end
-
-  describe "#cobble" do
-    let(:shoe) { Shoe.new("Nike") }
-
-    it "says that the shoe has been repaired" do
-      expect { shoe.cobble }.to output("The shoe has been repaired.\n").to_stdout
-    end
-
-    it "makes the shoe's condition new" do
-      shoe.cobble
-      expect(shoe.condition).to eq("new")
-    end
-  end
+def cobble 
+    @brand = "Nike"
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+end
 end
