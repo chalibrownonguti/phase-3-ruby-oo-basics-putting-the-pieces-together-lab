8882cb34a47ef3671e154f564d156f9fdd4c9894
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-19 08:49:29 +0300

    updates

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
-
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+  
+    def initialize(title)
+      @title = title
+    end
+  
+    def turn_page
+      # Implement the logic for turning the page here
+    end
+  end
+  
 
